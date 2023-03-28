865a607c5bc0eb6ccb31a1bb234703aa484bc26f
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-03-28 14:25:27 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { GlobalStyle } from '../GlobalStyle';
+import { Container } from './Container.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <GlobalStyle />
+    </Container>
   );
 };
