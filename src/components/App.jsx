4a5304247f980f25bd5c3cf221f09e5506bed52c
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-03-29 13:28:50 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import { Component } from 'react';
 import { GlobalStyle } from '../GlobalStyle';
 import { Container } from './Container.styled';
+import {} from './Button/Button';
+import {} from './ImageGallery/ImageGallery';
+import {} from './ImageGalleryItem/ImageGalleryItem';
+import {} from './Loader/Loader';
+import {} from './Modal/Modal';
+import {} from './Searchbar/Searchbar';
 
 export class App extends Component {
   state = {
